317fa15bfcee6ddeb53c57e9fc71efa374d2cf21
poorvarathore05 <poorvarathore05@gmail.com>
2021-04-24 17:31:38 +0530

    Added check-word route and modified index.html

--- flask_boggle/app.py
+++ flask_boggle/app.py
@@ -1,10 +1,11 @@
 from boggle import Boggle
-from flask import Flask,render_template
+from flask import Flask,render_template, request, session, jsonify
 
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'boggle_secret'
 
 @app.route('/')
 def start_page():
@@ -13,4 +14,16 @@ def start_page():
 @app.route('/start')
 def boggle_page():
     make_board = boggle_game.make_board()
-    return render_template('table.html',board = make_board)
+    session['board'] = make_board
+    high_score = session.get('high_score',0)
+    nplays = session.get('nplayes',0)
+    return render_template('index.html',board = make_board ,high_score=high_score,nplays=nplays)
+
+@app.route('/check-word')
+def check_word():
+    word  = request.args('word')
+    board = session['board']
+    response = boggle_game.check_valid_word(board,word)
+
+    return jsonify({'result' : response})
+
