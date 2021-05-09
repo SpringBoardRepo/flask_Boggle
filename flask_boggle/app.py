07f99ca72e77390030a65ad2c1e7a215772200ae
Rathore <RahulSingh.Rathore@in.pega.com>
2021-05-08 19:49:46 -0700

    added other routes and .js

--- flask_boggle/app.py
+++ flask_boggle/app.py
@@ -16,7 +16,7 @@ def boggle_page():
     make_board = boggle_game.make_board()
     session['board'] = make_board
     high_score = session.get('high_score',0)
-    nplays = session.get('nplayes',0)
+    nplays = session.get('nplays',0)
     return render_template('index.html',board = make_board ,high_score=high_score,nplays=nplays)
 
 @app.route('/check-word')
@@ -27,3 +27,14 @@ def check_word():
 
     return jsonify({'result' : response})
 
+@app.route('/post-score', methods = ['POST'])
+def post_score():
+
+    score = request.json['score']
+    highscore = session.get('highscore',0)
+    nplays = session.get('nplays',0)
+
+    session['nplays'] = nplays+1
+    session['highscore'] = max(score,highscore)
+
+    return jsonify(brokerecord=score>highscore)
